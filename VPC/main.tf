04d3e2832fb140ee314e5e482de4ed7c4daa7ec2
Keerthibb <92278245+Keerthibb@users.noreply.github.com>
2024-04-27 03:51:07 -0700

    Update main.tf

--- VPC/main.tf
+++ VPC/main.tf
@@ -1,7 +1,8 @@
-module "NV" {
-    source = "./module/vpc_module"
-    providers = {
-      aws = aws.NV
-    }
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "VPC_creation" {
+  source = "./module/vpc_module"
   vpc_cidr = "10.0.0.0/26"
 }
